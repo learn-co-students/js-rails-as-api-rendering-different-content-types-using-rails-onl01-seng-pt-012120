9500ad2f7c0afbc8bd22902f2d30bf18e64e5053
Charlie Pelton <charliejp0311@gmail.com>
2020-08-07 19:56:35 -0500

    Done.

--- .history/app/controllers/birds_controller_20200807193004.rb
+++ .history/app/controllers/birds_controller_20200807193004.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    render json: { message: 'Hashes of data will get converted to JSON' }
   end
 end
